78989977cdd95fc4b0bfa25aee7164a53ae53c89
@Vardanyan <nanevardanyants@gmail.com>
2017-05-26 19:53:20 +0400

    Post Data Model added.

---
+++ src/main/java/dataModel/Post.java
@@ -0,0 +1,81 @@
+package dataModel;
+
+import java.util.Date;
+
+/**
+ * Created by nane on 5/26/17.
+ */
+public class Post {
+    private long postId;
+    private long commentId;
+    private long userId;
+    private long appAreaId;
+    private Date timestamp;
+    private String title;
+    private String content;
+    private boolean isCorrect;
+
+    public long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(long postId) {
+        this.postId = postId;
+    }
+
+    public long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(long commentId) {
+        this.commentId = commentId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getAppAreaId() {
+        return appAreaId;
+    }
+
+    public void setAppAreaId(long appAreaId) {
+        this.appAreaId = appAreaId;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+}
